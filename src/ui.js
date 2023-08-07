4ebf9f66872fc39772fef306449aaae3e7c2e3e8
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-08-07 18:09:11 -0400

    removed data-target for navBtns

--- src/ui.js
+++ src/ui.js
@@ -92,17 +92,38 @@ export const uiHandler = (container) => {
         const navbar = document.createElement('div');
         navbar.classList.add('navbar');
 
+        // a navEntry element for each list; allows access and deletion of list
         _collection.lists.forEach(list => {
+            const navEntry = document.createElement('div');
+            navEntry.classList.add('navEntry');
+            
+            // creation of button to open/load lists
             const navBtn = document.createElement('button');
             navBtn.classList.add('nav-btn');
             navBtn.setAttribute('data-target', list.name);
             navBtn.textContent = list.name;
             navBtn.addEventListener('click', () => {
-                setCurrentList(_collection.findList(navBtn.getAttribute('data-target')))
+                setCurrentList(list)
                 unloadList();
                 loadList();
             });
-            navbar.appendChild(navBtn);
+            navEntry.appendChild(navBtn);
+
+            // // creation of button to delete list
+            // const deleteListBtn = document.createElement('button');
+            // deleteListBtn.classList.add('delete-list-btn');
+            // deleteListBtn.setAttribute('data-target', list.name);
+            // deleteListBtn.textContent = 'X';
+            // deleteListBtn.addEventListener('click', (e) => {
+            //     e.preventDefault();
+            //     _collection.deleteList(list.name);
+            //     unloadSidebar();
+            //     loadSidebar();
+
+            // })
+            // navEntry.appendChild(deleteListBtn);
+
+            navbar.appendChild(navEntry);
         })
 
         return navbar;
